41cbae51af5415144cfe35a8cb25318ad1787a0e
Daniel Cai <legoguy145@gmail.com>
2021-01-21 22:13:52 -0500

    Implemented item-container to queue

--- app/destiny-api-ripper-gui/src/renderer.js
+++ app/destiny-api-ripper-gui/src/renderer.js
@@ -3,19 +3,27 @@ let fs = require('fs');
 let itemContainer = $('#item-container');
 let queue = $('#extract-queue');
 let queueButtons = $('#queue-buttons');
+let searchBox = $('#search-box');
 
 items = JSON.parse(fs.readFileSync('./data/item_definitions.json'));
 
 function createItemTile(item) {
     let tileRoot = $('<div></div>', {
-        class: 'item-tile d-flex align-items-center p-1', 
+        class: 'item-tile d-flex align-items-center p-1 rounded', 
         id: item.hash,
+        'data-index': item.index,
+        on: {
+            click: itemTileClickHandler
+        }
     });
-    tileRoot.append($('<img></img>', {
-        src: `https://bungie.net${item.displayProperties.icon}`,
-        referrerpolicy: 'no-referrer',
-        crossorigin: 'None'
-    }));
+
+    if (item.displayProperties.hasIcon){
+        tileRoot.append($('<img></img>', {
+            src: `https://bungie.net${item.displayProperties.icon}`,
+            referrerpolicy: 'no-referrer',
+            crossorigin: 'None'
+        }));
+    }
 
     let textDiv = $('<div></div>', {
         class: 'tile-text d-inline-flex p-2'
@@ -39,9 +47,34 @@ function createItemTile(item) {
     return tileRoot;
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
+function itemTileClickHandler(event){
+    // itemTile.detach()
+    if ($(event.currentTarget).parents()[0].id == 'item-container'){
+        queue.append($(event.currentTarget).detach());
+    } else if ($(event.currentTarget).parents()[0].id == 'extract-queue') {
+        //TODO: Put the item back in the correct spot
+        itemContainer.append($(event.currentTarget).detach());
+    }
+}
+
+function loadItems(){
+    itemContainer.empty();
+    queue.empty();
     console.log(`${items.length} items indexed.`)
     items.forEach(item => {
-        itemContainer.append(createItemTile(item))
+        itemContainer.append(createItemTile(item));
     });
+    console.log(`${items.length} items loaded.`)
+}
+
+function executeQueue(){
+    let exportQueue = [...queue[0].children];
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    loadItems();
 });
+
+document.getElementById('reset-button').addEventListener('click', loadItems);
+document.getElementById('queue-clear-button').addEventListener('click', loadItems);
+document.getElementById('queue-execute-button').addEventListener('click', executeQueue);

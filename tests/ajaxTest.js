f43a50272fa149dabee8d017fd5bc8f87b586ddb
Daniel Cai <legoguy145@gmail.com>
2021-01-24 00:40:21 -0500

    more tests

--- tests/ajaxTest.js
+++ tests/ajaxTest.js
@@ -1,4 +1,4 @@
-const $ = require('jquery');
+const https = require('https');
 
 let manifest
 let jsonWorldContent
@@ -7,12 +7,16 @@ let baseURL = 'https://www.bungie.net';
 let apiRoot = baseURL + '/Platform';
 let endpointPath = '/Destiny2/Manifest/';
 
-$.ajax({
-    type: 'GET',
-    url: apiRoot + endpointPath,
-    headers: {
-        'X-API-Key': 'cd4e01bd37b74c789af6a1479fa62801'
-    }
-}).then((res) => {
-    manifest = res;
-});
+https.get(apiRoot + endpointPath, {
+    
+})
+
+// $.ajax({
+//     type: 'GET',
+//     url: apiRoot + endpointPath,
+//     headers: {
+//         'X-API-Key': 'cd4e01bd37b74c789af6a1479fa62801'
+//     }
+// }).then((res) => {
+//     manifest = res;
+// });

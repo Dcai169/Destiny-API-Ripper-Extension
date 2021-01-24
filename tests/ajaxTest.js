8716c574ed11d146428a2f99a90235ea80df4a2b
Daniel Cai <legoguy145@gmail.com>
2021-01-24 17:58:20 -0500

    testing

--- tests/ajaxTest.js
+++ tests/ajaxTest.js
@@ -7,9 +7,13 @@ let baseURL = 'https://www.bungie.net';
 let apiRoot = baseURL + '/Platform';
 let endpointPath = '/Destiny2/Manifest/';
 
-https.get(apiRoot + endpointPath, {
-    
-})
+https.get(apiRoot + endpointPath, { headers: { 'X-API-Key': 'cd4e01bd37b74c789af6a1479fa62801' } }, (res) => {
+    console.log(res.statusCode);
+
+    res.on('data', d => { manifest += d });
+
+    // res.on('end', () => { manifest = JSON.parse(manifest); console.log(manifest); });
+}).on('finish', () => { console.log(manifest); });
 
 // $.ajax({
 //     type: 'GET',

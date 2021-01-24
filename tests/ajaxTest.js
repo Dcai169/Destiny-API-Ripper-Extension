4df2fda1883a780f7709490978243c2827c26682
Daniel Cai <legoguy145@gmail.com>
2021-01-24 00:39:47 -0500

    more tests

---
+++ tests/ajaxTest.js
@@ -0,0 +1,18 @@
+const $ = require('jquery');
+
+let manifest
+let jsonWorldContent
+
+let baseURL = 'https://www.bungie.net';
+let apiRoot = baseURL + '/Platform';
+let endpointPath = '/Destiny2/Manifest/';
+
+$.ajax({
+    type: 'GET',
+    url: apiRoot + endpointPath,
+    headers: {
+        'X-API-Key': 'cd4e01bd37b74c789af6a1479fa62801'
+    }
+}).then((res) => {
+    manifest = res;
+});

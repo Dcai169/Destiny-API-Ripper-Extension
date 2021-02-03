b267100fffddb905179ebd4e397b6f0215103e9e
Daniel Cai <legoguy145@gmail.com>
2021-02-03 00:45:45 -0500

    implemented D1 item tile generation

--- app/destiny-api-ripper-gui/src/scripts/itemTile.js
+++ app/destiny-api-ripper-gui/src/scripts/itemTile.js
@@ -1,58 +1,106 @@
 function createItemTile(item, game) {
-    let tileRoot = $('<div></div>', {
-        class: 'item-tile d-flex align-items-center p-1',
-        style: 'position: relative;',
-        id: item.hash,
-        'data-index': item.index,
-        name: (item.displayProperties.name ? item.displayProperties.name : 'Classified'),
-        on: {
-            click: itemTileClickHandler
-        }
-    });
-
-    // Image div (Icon & Season Badge)
-    let imgDiv = $('<div></div>', {
-        style: 'display: grid; position: relative;'
-    });
-
-    imgDiv.append($('<img></img>', {
-        src: `https://bungie.net${item.displayProperties.icon}`,
-        referrerpolicy: 'no-referrer',
-        crossorigin: 'None',
-        loading: 'lazy',
-        style: 'grid-row: 1; grid-column: 1;'
-    }));
-
-    if (item.iconWatermark) {
+    let tileRoot;
+    if (game === '2') {
+        tileRoot = $('<div></div>', {
+            class: 'item-tile d-flex align-items-center p-1',
+            style: 'position: relative;',
+            id: item.hash,
+            'data-index': item.index,
+            name: (item.displayProperties.name ? item.displayProperties.name : 'Classified'),
+            on: {
+                click: itemTileClickHandler
+            }
+        });
+    
+        // Image div (Icon & Season Badge)
+        let imgDiv = $('<div></div>', {
+            style: 'display: grid; position: relative;'
+        });
+    
         imgDiv.append($('<img></img>', {
-            src: `https://bungie.net${item.iconWatermark}`,
+            src: `https://bungie.net${item.displayProperties.icon}`,
             referrerpolicy: 'no-referrer',
             crossorigin: 'None',
             loading: 'lazy',
-            style: 'grid-row: 1; grid-column: 1; z-index: 1;'
+            style: 'grid-row: 1; grid-column: 1;'
         }));
+    
+        if (item.iconWatermark) {
+            imgDiv.append($('<img></img>', {
+                src: `https://bungie.net${item.iconWatermark}`,
+                referrerpolicy: 'no-referrer',
+                crossorigin: 'None',
+                loading: 'lazy',
+                style: 'grid-row: 1; grid-column: 1; z-index: 1;'
+            }));
+        }
+    
+        // Item text (Name & Type)
+        let textDiv = $('<div></div>', {
+            class: 'tile-text d-inline-flex px-2'
+        });
+    
+        let textContainer = $('<div></div>', {});
+        textContainer.append($(`<h6></h6>`, {
+            text: (item.displayProperties.name ? item.displayProperties.name : undefined),
+            class: 'm-0',
+            style: `color: var(--${hashToRarityName(item.inventory.tierTypeHash)}-color)`
+        }));
+        textContainer.append($('<i></i>', {
+            text: (item.itemTypeDisplayName ? item.itemTypeDisplayName : undefined),
+            class: 'fs-5 item-type',
+            // style: 'font-size: 110%'
+        }));
+    
+        textDiv.append(textContainer);
+        tileRoot.append(imgDiv);
+        tileRoot.append(textDiv);
+    } else if (game === '1') {
+        tileRoot = $('<div></div>', {
+            class: 'item-tile d-flex align-items-center p-1',
+            style: 'position: relative;',
+            id: item.hash,
+            'data-index': item.index,
+            name: (item.itemName ? item.itemName : 'Classified'),
+            on: {
+                click: itemTileClickHandler
+            }
+        });
+    
+        // Image div (Icon & Season Badge)
+        let imgDiv = $('<div></div>', {
+            style: 'display: grid; position: relative;'
+        });
+    
+        imgDiv.append($('<img></img>', {
+            src: `https://bungie.net${(item.icon ? item.icon : '/img/misc/missing_icon.png')}`,
+            referrerpolicy: 'no-referrer',
+            crossorigin: 'None',
+            loading: 'lazy',
+            style: 'grid-row: 1; grid-column: 1;'
+        }));
+    
+        // Item text (Name & Type)
+        let textDiv = $('<div></div>', {
+            class: 'tile-text d-inline-flex px-2'
+        });
+    
+        let textContainer = $('<div></div>', {});
+        textContainer.append($(`<h6></h6>`, {
+            text: (item.itemName ? item.itemName : undefined),
+            class: 'm-0',
+            style: `color: var(--${tierNumberToRarityName(item.tierType)}-color)`
+        }));
+        textContainer.append($('<i></i>', {
+            text: (item.itemTypeName ? item.itemTypeName : undefined),
+            class: 'fs-5 item-type',
+            // style: 'font-size: 110%'
+        }));
+    
+        textDiv.append(textContainer);
+        tileRoot.append(imgDiv);
+        tileRoot.append(textDiv);
     }
-
-    // Item text (Name & Type)
-    let textDiv = $('<div></div>', {
-        class: 'tile-text d-inline-flex px-2'
-    });
-
-    let textContainer = $('<div></div>', {});
-    textContainer.append($(`<h6></h6>`, {
-        text: (item.displayProperties.name ? item.displayProperties.name : undefined),
-        class: 'm-0',
-        style: `color: var(--${hashToRarityName(item.inventory.tierTypeHash)}-color)`
-    }));
-    textContainer.append($('<i></i>', {
-        text: (item.itemTypeDisplayName ? item.itemTypeDisplayName : undefined),
-        class: 'fs-5 item-type',
-        // style: 'font-size: 110%'
-    }));
-
-    textDiv.append(textContainer);
-    tileRoot.append(imgDiv);
-    tileRoot.append(textDiv);
 
     return tileRoot;
 }
@@ -60,8 +108,10 @@ function createItemTile(item, game) {
 function itemTileClickHandler(event) {
     let tileLocation = $(event.currentTarget).eq(0).parents().attr('id');
     if (tileLocation === 'item-container') {
+        console.log(`${$(event.currentTarget).eq(0).attr('id')} added to queue.`);
         queue.append($(event.currentTarget).detach());
     } else if (tileLocation === 'extract-queue') {
+        console.log(`${$(event.currentTarget).eq(0).attr('id')} returned to container.`);
         addItemToContainer($(event.currentTarget).detach());
     }
 }
@@ -90,6 +140,28 @@ function hashToRarityName(hash) {
             return 'uncommon';
 
         case 3340296461:
+            return 'common';
+        
+        default:
+            break;
+    }
+}
+
+function tierNumberToRarityName(tierType) {
+    switch (tierType) {
+        case 6:
+            return 'exotic';
+        
+        case 5:
+            return 'legendary';
+        
+        case 4:
+            return 'rare';
+
+        case 3:
+            return 'uncommon';
+
+        case 2:
             return 'common';
         
         default:

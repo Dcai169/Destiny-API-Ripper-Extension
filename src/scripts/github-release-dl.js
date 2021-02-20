6c592a4a7df3e6fd9a40b41c4c69136351d66dc5
Daniel Cai <legoguy145@gmail.com>
2021-02-20 01:01:50 -0500

    added some elements to loading window

--- src/scripts/github-release-dl.js
+++ src/scripts/github-release-dl.js
@@ -1,8 +1,26 @@
 const axios = require('axios');
 const fs = require('fs');
-const path = require('path');
 const sevenBin = require('7zip-bin');
 const { extractFull } = require('node-7z');
+
+function getDownloadUrl(acceptPrerelease=false) {
+    // Get releases
+    axios.get('https://api.github.com/repos/TiredHobgoblin/Destiny-Collada-Generator/releases').then((res) => {
+        // determine which release to use
+        let targetRelease = 0;
+        if (!acceptPrerelease) {
+            res.data.some((v, i) => { targetRelease = i; return !v.prerelease; });
+        }
+        // get asset definitions
+        axios.get(res.data[targetRelease].assets_url).then((res) => {
+            for (const i of res.data) {
+                if (i.name.toLowerCase().includes((process.platform === 'darwin' ? 'osx' : process.platform.substring(0, 3)))) {
+                    return i.browser_download_url;
+                }
+            }
+        });
+    });
+}
 
 async function downloadFile(dlUrl, dlPath) {
     const writer = fs.createWriteStream(dlPath);
@@ -20,26 +38,8 @@ async function downloadFile(dlUrl, dlPath) {
     });
 }
 
-function downloadAndExtract(dlPath, acceptPrerelease) {
-    // Get releases
-    axios.get('https://api.github.com/repos/TiredHobgoblin/Destiny-Collada-Generator/releases').then((res) => {
-        // determine which release to use
-        let targetRelease = 0;
-        if (!acceptPrerelease) {
-            res.data.some((v, i) => { targetRelease = i; return !v.prerelease; });
-        }
-        // get asset definitions
-        axios.get(res.data[targetRelease].assets_url).then((res) => {
-            let downloadUrl = (() => {
-                for (const i of res.data) {
-                    if (i.name.toLowerCase().includes((process.platform === 'darwin' ? 'osx' : process.platform.substring(0, 3)))) {
-                        return i.browser_download_url;
-                    }
-                }
-            })();
-            downloadFile(downloadUrl, path.join(dlPath, path.parse(downloadUrl).base)).then(() => {
-                
-            });
-        });
-    });
+function extract7zip(archivePath) {
+    const extractorStream = extractFull(archivePath, path.parse(archivePath).dir, {$bin: sevenBin.path7za, recursive: true})
 }
+
+// downloadFile(downloadUrl, path.join(dlPath, path.parse(downloadUrl).base)).then(() => {});

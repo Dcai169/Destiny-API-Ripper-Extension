9ba51084b0852c41633dbbfc4befdee60a524bbb
Daniel Cai <legoguy145@gmail.com>
2021-02-25 09:30:21 -0500

    added redundancy check

--- src/main/scripts/uiUtils.js
+++ src/main/scripts/uiUtils.js
@@ -1,7 +1,10 @@
 function setVisibility(jqueryObj, state) {
     // true -> visible
     // false -> hidden
-    jqueryObj.removeClass((state ? 'hidden' : 'm-1')).addClass((state ? 'm-1' : 'hidden'))
+    
+    if (jqueryObj.hasClass((state ? 'hidden' : 'm-1'))) {
+        jqueryObj.removeClass((state ? 'hidden' : 'm-1')).addClass((state ? 'm-1' : 'hidden'));
+    }
 }
 
 function updateUIInput(elementId, value) {

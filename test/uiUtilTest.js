051c4dc737c89cfd954776b30b28402ea4f8253a
Daniel Cai <legoguy145@gmail.com>
2021-02-25 09:39:15 -0500

    tests are hard pt2

--- test/uiUtilTest.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const { setVisibility } = require('../src/main/scripts/uiUtils.js');
-
-describe('uiUtils.js', () => {
-    describe('setVisibility', () => {
-        it('should assign classes correctly.', () => {
-        });
-    });
-});

018ba2000072c0ac594300f357c24c7f229ea874
Daniel Cai <legoguy145@gmail.com>
2021-12-29 16:50:56 -0500

    simplified inclusion criteria

--- src/main/scripts/destinyManifest.js
+++ src/main/scripts/destinyManifest.js
@@ -3,11 +3,8 @@ const axios = require('axios');
 // Read API Key
 require('dotenv').config({ path: 'api.env' });
 
-const baseUrl = 'https://bungie.net';
-const blacklistedDestiny1Hashes = [4248210736]; // Default Shader
-const blacklistedDestiny2Hashes = [4248210736, 2426387438, 2931483505, 1959648454, 702981643, 2325217837] + // Default Shader
-    [2965439266, 4236468733, 2699000684, 1702504372, 3344732822, 2912265353, 4143534670, 873770815, 3367964921, 4089988225, 811724212, 3054638345, 463166592, 3507818312, 3835954362, 1339405989] + // Solstice Glows
-    [3807544519, 834178986, 839740147, 577345565, 574694085, 2039333456, 60802325, 3031612900, 2449203932, 242730894, 3735037521, 558870048, 2419910641, 2552954151, 2251060291, 3692806198]; // More Glows
+const BASE_URL = 'https://bungie.net';
+const DEFAULT_SHADER_HASH = 4248210736; // Default Shader
 
 function getDestiny1ItemDefinitions(locale = 'en') {
     return new Promise((resolve, reject) => {
@@ -18,13 +15,11 @@ function getDestiny1ItemDefinitions(locale = 'en') {
                 for (const [hash, item] of Object.entries(res.data)) {
                     // Filter items in the manifest
                     if (((item) => {
-                        if (blacklistedDestiny1Hashes.includes(item.hash)) { return false }
-                        if (arrayEquals(item.itemCategoryHashes, [23, 38, 20])) { return false } // Hunter Artifacts
-                        if (arrayEquals(item.itemCategoryHashes, [22, 38, 20])) { return false } // Titan Artifacts
-                        if (arrayEquals(item.itemCategoryHashes, [21, 38, 20])) { return false } // Warlock Artifacts
-                        if (item.itemCategoryHashes && item.itemCategoryHashes.includes(1) && item.itemCategoryHashes.length === 2) { return false } // Reforge Weapon
-                        if (arrayEquals(item.itemCategoryHashes, [41, 52])) { return true } // Shaders
-                        if (arrayEquals(item.itemCategoryHashes, [42, 52])) { return true } // Ships
+                        if (item.hash === DEFAULT_SHADER_HASH) { return false }
+                        if (item.itemCategoryHashes?.includes(38) && item.itemCategoryHashes?.includes(20)) { return false } // Armor Artifacts
+                        if (item.itemCategoryHashes?.includes(1) && item.itemCategoryHashes?.length === 2) { return false } // Reforge Weapon
+                        if (item.itemCategoryHashes?.includes(41) && item.itemCategoryHashes?.includes(52)) { return true } // Shaders
+                        if (item.itemCategoryHashes?.includes(42) && item.itemCategoryHashes?.includes(52)) { return true } // Ships
                         if ([2, 3].includes(item.itemType)) { return true } // Armor and Weapons
                     })(item)) {
                         destiny1ItemDefinitions[hash] = item;
@@ -45,17 +40,17 @@ function getDestiny2ItemDefinitions(locale = 'en') {
     return new Promise((resolve, reject) => {
         let destiny2ItemDefinitions = {};
         // Send GET request to manifest server
-        axios.get(baseUrl + '/Platform/Destiny2/Manifest/', { headers: { 'X-API-Key': process.env.API_KEY } })
+        axios.get(BASE_URL + '/Platform/Destiny2/Manifest/', { headers: { 'X-API-Key': process.env.API_KEY } })
             .then((res) => {
                 // Send GET for item definitions
-                axios.get(baseUrl + res.data.Response.jsonWorldComponentContentPaths[locale].DestinyInventoryItemDefinition)
+                axios.get(BASE_URL + res.data.Response.jsonWorldComponentContentPaths[locale].DestinyInventoryItemDefinition)
                     .then((res) => {
                         for (let [hash, item] of Object.entries(res.data)) {
                             if (((item) => {
-                                if (blacklistedDestiny2Hashes.includes(item.hash)) { return false }
-                                if ([2, 21, 22, 24].includes(item.itemType)) { return true } // Armor, Ships, Sparrows, Ghost Shells
-                                if (item.defaultDamageType > 0) { return true } // Weapons
-                                if (item.itemType === 19 && [20, 21].includes(item.itemSubType)) { return true } // Ornaments
+                                if (item.hash === DEFAULT_SHADER_HASH) { return false }
+                                if (!item?.displayProperties.name) { return false }
+                                if (item.itemCategoryHashes?.some((itemCategoryHash) => { return [59, 3109687656].includes(itemCategoryHash) })) { return false } // Mods and ?
+                                if (item.itemCategoryHashes?.some((itemCategoryHash) => { return [1, 20, 39, 41, 42, 43, 55].includes(itemCategoryHash) })) { return true } // Armor, weapons, shaders, ships, ghost shells, and sparrows
                             })(item)) {
                                 destiny2ItemDefinitions[hash] = item;
                             }
@@ -70,12 +65,6 @@ function getDestiny2ItemDefinitions(locale = 'en') {
                     }).catch(reject);
             }).catch(reject);
     });
-}
-
-function arrayEquals(a, b) {
-    return Array.isArray(a) && Array.isArray(b) && // both are Arrays
-        a.length === b.length && // Same length
-        a.every((val, index) => val === b[index]); // each value is the same
 }
 
 module.exports = { getDestiny1ItemDefinitions, getDestiny2ItemDefinitions };

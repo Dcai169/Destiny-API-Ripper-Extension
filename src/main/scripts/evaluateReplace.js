ec21f3cd16a68bf23b604069be5da171dfd58c6d
Daniel Cai <legoguy145@gmail.com>
2021-12-30 00:50:25 -0500

    Removed vestigial code

--- src/main/scripts/evaluateReplace.js
+++
@@ -1,5 +0,0 @@
-function evaluateReplace(expression, { replacement = undefined, callback = (res) => { return res; }, evalTarget = false } = {}) {
-    return (!!expression === evalTarget ? replacement : callback(expression));
-}
-
-module.exports = evaluateReplace;

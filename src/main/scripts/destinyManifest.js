642e8d5bd00375b3cbb992b0e6f72e25db1847d9
Daniel Cai <legoguy145@gmail.com>
2022-01-07 19:46:10 -0500

    improved D1 item filtering

--- src/main/scripts/destinyManifest.js
+++ src/main/scripts/destinyManifest.js
@@ -9,7 +9,7 @@ const DESTINY2_HASH_BLACKLIST = [702981643, 2965439266, 2426387438, 3807544519, 
 
 function getDestiny1ItemDefinitions(locale = 'en') {
     return new Promise((resolve, reject) => {
-        let destiny1ItemDefinitions = {};
+        let d1ItemDefinitions = {};
         // Send GET request to manifest server
         axios.get(`https://dare-manifest-server.herokuapp.com/manifest?locale=${locale}`)
             .then((res) => {
@@ -17,29 +17,27 @@ function getDestiny1ItemDefinitions(locale = 'en') {
                     // Filter items in the manifest
                     if (((item) => {
                         if (item.hash === DEFAULT_SHADER_HASH) { return false }
-                        if (item.itemCategoryHashes?.includes(38) && item.itemCategoryHashes?.includes(20)) { return false } // Armor Artifacts
                         if (item.itemCategoryHashes?.includes(1) && item.itemCategoryHashes?.length === 2) { return false } // Reforge Weapon
-                        if (item.itemCategoryHashes?.includes(41) && item.itemCategoryHashes?.includes(52)) { return true } // Shaders
-                        if (item.itemCategoryHashes?.includes(42) && item.itemCategoryHashes?.includes(52)) { return true } // Ships
-                        if ([2, 3].includes(item.itemType)) { return true } // Armor and Weapons
+                        if (item.itemCategoryHashes?.some((itemCategoryHash) => { return [15, 38, 51].includes(itemCategoryHash) })) { return false } // Blueprints and artifacts
+                        if (item.itemCategoryHashes?.some((itemCategoryHash) => { return [1, 20, 39, 41, 42, 43, 55].includes(itemCategoryHash) })) { return true } // Armor, weapons, shaders, ships, ghost shells, and sparrows
                     })(item)) {
-                        destiny1ItemDefinitions[hash] = item;
+                        d1ItemDefinitions[hash] = item;
                     }
                 }
                 // Sort into a Map Object
-                let items = Object.values(destiny1ItemDefinitions);
-                destiny1ItemDefinitions = new Map();
+                let items = Object.values(d1ItemDefinitions).sort((a, b) => { return a.index - b.index });
+                d1ItemDefinitions = new Map();
                 items.forEach((item) => {
-                    destiny1ItemDefinitions.set(item.hash, item);
+                    d1ItemDefinitions.set(item.hash, item);
                 });
-                resolve(destiny1ItemDefinitions);
+                resolve(d1ItemDefinitions);
             }).catch(reject);
     });
 }
 
 function getDestiny2ItemDefinitions(locale = 'en') {
     return new Promise((resolve, reject) => {
-        let destiny2ItemDefinitions = {};
+        let d2ItemDefinitions = {};
         // Send GET request to manifest server
         axios.get(BASE_URL + '/Platform/Destiny2/Manifest/', { headers: { 'X-API-Key': process.env.API_KEY } })
             .then((res) => {
@@ -54,16 +52,16 @@ function getDestiny2ItemDefinitions(locale = 'en') {
                                 if (item.itemCategoryHashes?.includes(3109687656)) { return false } // Mods
                                 if (item.itemCategoryHashes?.some((itemCategoryHash) => { return [1, 20, 39, 41, 42, 43, 55, 1742617626, 3124752623].includes(itemCategoryHash) })) { return true } // Armor, weapons, shaders, ships, ghost shells, and sparrows
                             })(item)) {
-                                destiny2ItemDefinitions[hash] = item;
+                                d2ItemDefinitions[hash] = item;
                             }
                         }
                         // Sort into a Map object
-                        let items = Object.values(destiny2ItemDefinitions).sort((a, b) => { return a.index - b.index });
-                        destiny2ItemDefinitions = new Map();
+                        let items = Object.values(d2ItemDefinitions).sort((a, b) => { return a.index - b.index });
+                        d2ItemDefinitions = new Map();
                         items.forEach((item) => {
-                            destiny2ItemDefinitions.set(item.hash, item);
+                            d2ItemDefinitions.set(item.hash, item);
                         });
-                        resolve(destiny2ItemDefinitions);
+                        resolve(d2ItemDefinitions);
                     }).catch(reject);
             }).catch(reject);
     });
